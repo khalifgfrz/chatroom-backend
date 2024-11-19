21e09d01489e2be1482e8d6736a5bbf481b5cde7
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-11-19 13:20:45 +0700

    settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,12 +73,6 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_cable.url = ENV['CABLE_URL']
+  config.action_cable.allowed_request_origins = [ENV['CABLE_ALLOWED_ORIGIN']]
 end
