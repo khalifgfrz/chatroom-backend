a792ae0651326976f3e49ef0bab8e9022b541506
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-11-19 17:09:13 +0700

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,5 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # config.action_cable.url = ENV['CABLE_URL']
-  # config.action_cable.allowed_request_origins = [ENV['CABLE_ALLOWED_ORIGIN']]
+  config.action_cable.disable_request_forgery_protection = true
 end
